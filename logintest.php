09d45e493debd3de384e7b25c545f59cbcbf287d
Nourdyn Vissers <nourdyn96@hotmail.com>
2013-12-03 11:30:10 +0100

    we hebben weer de loginClass aangepast en verkleind

---
+++ logintest.php
@@ -0,0 +1,22 @@
+<?php
+	require_once ('class/LoginClass.php');
+	
+	$loginClassObj = new LoginClass();
+	
+	$query = "SELECT * FROM 'login'";
+	
+	$result_array = $loginClassObj->find_by_sql($query);
+	
+	echo "<table>";
+	
+	foreach ( $result_array as $value)
+	{
+		echo "<tr>
+				<td>"$value->get_id()."<br>"</td>
+				<td>"$value->get_id()."<br>"</td>
+			  </tr>
+	}
+	echo "</table>";
+?>
+
+	
