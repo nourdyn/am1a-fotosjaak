29dcc4cfdba3199c93dabf6134cb22d282d2bae6
Nourdyn Vissers <nourdyn96@hotmail.com>
2013-12-03 10:49:44 +0100

    de login files voor de database en login test gemaakt en verder afgewerkt vandaag

---
+++ LoginClass.php
@@ -0,0 +1,46 @@
+<?php 
+	require_once ("MySqlDatabaseClass.php");
+	
+	class LoginClass
+	{
+		//fields
+		private $id;
+		private $email;
+		private $password;
+		private $userrole;
+		private $activated;
+		private $activationdate;
+		
+		//constructor 
+		public function __construct()
+		{
+			
+			
+		}
+		public function find_by_sql($query)
+		{
+			global $database;
+			$result = $database->fire_query($query);
+			
+			$object_array = array();
+			
+			while ( $row = mysql_fetch_array($result))
+			{
+				$object = new LoginClass();
+				$object->id    				= $row['id'];
+				$object->email 				= $row['email'];
+				$object->password			= $row['password'];
+				$object->userrole			= $row['userrole'];
+				$object->activated			= $row['activated'];
+				$object->activationdate		= $row['activationdate'];
+				
+				$object_array[] = $object;
+				
+				
+			}
+			return $object_array;
+			
+			
+		}
+	}
+?>
